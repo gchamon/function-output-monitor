869afda0d25f40b3352b03b58b90d7a9d49a9645
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2022-09-11 11:19:19 -0300

    use Alarm with block instead of direct function calls

--- function_output_monitor/monitor.py
+++ function_output_monitor/monitor.py
@@ -9,28 +9,18 @@ class MonitorTimeoutError(Exception):
     pass
 
 
-class NeitherTimeoutNorStopConditionError(Exception):
-    def __init__(self):
-        super().__init__("This error should never be raised")
-
-
 def monitor_function_output(function_to_monitor: Callable[[], T],
                             stop_condition: Callable[[T], bool],
                             interval: NumberType,
                             timeout: NumberType,
                             on_timeout: Optional[Callable[[], None]] = None) -> T:
-    alarm = Alarm(timeout)
-    alarm.start()
-    while stop_condition(return_value := function_to_monitor()) is not True and not alarm.alarmed:
-        alarm.wait(interval)
+    with Alarm(timeout) as alarm:
+        while not stop_condition(return_value := function_to_monitor()) and not alarm.alarmed:
+            alarm.wait(interval)
 
     if stop_condition(return_value):
-        alarm.reset()
         return return_value
 
-    if alarm.alarmed:
-        if on_timeout is not None:
-            on_timeout()
-        raise MonitorTimeoutError()
-
-    raise NeitherTimeoutNorStopConditionError()
+    if on_timeout is not None:
+        on_timeout()
+    raise MonitorTimeoutError()
